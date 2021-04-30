3f27a178c51857996a7ad976622be1f1f48fd0b4
Gagan Malvi <malvi@aospa.co>
2021-04-30 19:32:58 +0200

    ims: Move Boot Jar defs to device
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- mtk-ims.mk
+++ mtk-ims.mk
@@ -21,16 +21,6 @@ PRODUCT_PACKAGES += \
     libmtk_vt_wrapper \
     libimsma_socketwrapper
 
-# Radio IMS Boot Jar
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
 # Radio IMS Jars
 PRODUCT_PACKAGES += \
     mediatek-common \
