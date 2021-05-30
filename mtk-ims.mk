7b4249a97afdd8cca05655c09afc0c7328d71b7b
Zinadin Zidan <zidan44@pixelexperience.org>
2021-05-30 14:09:45 +0600

    mtk-ims: vt_hal: Build videotelephony 1.0 impl
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Iea4ccf40b7e08b486d6af1976754e736e801bd5d

--- mtk-ims.mk
+++ mtk-ims.mk
@@ -36,7 +36,8 @@ PRODUCT_PACKAGES += \
 
 # Video Telephony
 PRODUCT_PACKAGES += \
-    vendor.mediatek.hardware.videotelephony@1.0
+    vendor.mediatek.hardware.videotelephony@1.0 \
+    vendor.mediatek.hardware.videotelephony@1.0-impl
 
 # Permissions
 PRODUCT_COPY_FILES += \
